875d9830ba610247cf56ab4f913a64bdcc3c9533
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-04 10:45:46 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_92718_RyannChandiari/week01/GameStore.kt
+++ src/oop_92718_RyannChandiari/week01/GameStore.kt
@@ -4,3 +4,6 @@ fun main() {
     val title = "Elden Ring"
     val price = 600000
 }
+
+// Logic: Jika > 500k bayar 80% (diskon 20%), selain itu bayar 90% (diskon 10%)
+fun calculateDiscount(price: Int) = if (price > 500000) (price * 0.8).toInt() else (price * 0.9).toInt()
