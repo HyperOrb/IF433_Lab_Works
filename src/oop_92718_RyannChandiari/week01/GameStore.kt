150491bbaa99d687c43e8ce0a2de11d9442d9956
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-04 10:47:00 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_92718_RyannChandiari/week01/GameStore.kt
+++ src/oop_92718_RyannChandiari/week01/GameStore.kt
@@ -4,17 +4,24 @@ fun main() {
     val title = "Elden Ring"
     val price = 600000
 
+    // CHECKPOINT 13: Variabel nullable
+    val userNote: String? = null
+
     val finalPrice = calculateDiscount(price)
 
-    // CHECKPOINT 12: Panggil fungsi dengan Named Arguments (wajib pakai title = ...)
-    printReceipt(title = title, finalPrice = finalPrice)
+    // Logika Elvis Operator: Jika userNote null, gunakan "Tidak ada catatan"
+    val finalNote = userNote ?: "Tidak ada catatan"
+
+    // Panggil fungsi dengan Named Arguments
+    printReceipt(title = title, finalPrice = finalPrice, note = finalNote)
 }
 
 fun calculateDiscount(price: Int) = if (price > 500000) (price * 0.8).toInt() else (price * 0.9).toInt()
 
-// Fungsi baru untuk cetak struk
-fun printReceipt(title: String, finalPrice: Int) {
+// Update fungsi untuk menerima parameter note
+fun printReceipt(title: String, finalPrice: Int, note: String) {
     println("=== STRUK BELANJA ===")
     println("Game: $title")
     println("Harga Akhir: Rp $finalPrice")
+    println("Catatan: $note")
 }
