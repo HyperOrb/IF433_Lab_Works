6752af73ef4812d7ec3a8ec4d02b870f090e70f2
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-04 10:36:40 +0700

    week01: implement grading logic using when expression

--- src/oop_92718_RyannChandiari/week01/GradingSystem.kt
+++ src/oop_92718_RyannChandiari/week01/GradingSystem.kt
@@ -7,4 +7,13 @@ fun main() {
 
     // REFACTOR: Pakai String Template ($variable) biar lebih ringkas
     println("Nama: $name, Nilai: $score")
+
+    // Checkpoint 3: Tambahkan logika when sebagai Expression
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+    println("Grade kamu: $grade")
 }
