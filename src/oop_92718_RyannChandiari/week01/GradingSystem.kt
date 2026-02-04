8c90a0840470cd258094001706772fc84fc682fa
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-04 10:33:43 +0700

    week01: initial setup and procedural code

---
+++ src/oop_92718_RyannChandiari/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_92718_RyannChandiari.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+    // Concatenation gaya lama
+    println("Nama: " + name + " Nilai: " + score)
+}
