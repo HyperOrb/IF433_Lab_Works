2c60033ffec5b2638e14c5d1131073089952a111
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-04 10:44:30 +0700

    week01: (tugas 1) clean up unused args and comments

--- src/oop_92718_RyannChandiari/week01/CircleCalculator.kt
+++ src/oop_92718_RyannChandiari/week01/CircleCalculator.kt
@@ -1,16 +1,13 @@
 package oop_92718_RyannChandiari.week01
 
-fun main(args: Array<String>) {
+fun main() { // Hapus parameter args
     val radius = 7.0
     val pi = 3.14
 
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
-
-    // REFACTOR: Panggil fungsi di dalam println
     println(checkSize(area))
 }
 
-// REFACTOR: Ubah jadi Expression Body (mengembalikan String)
 fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
