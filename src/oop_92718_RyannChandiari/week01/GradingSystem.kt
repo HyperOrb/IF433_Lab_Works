c3c96091924f46391d9e1c4aeb149bcb27ab37cf
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-04 10:37:34 +0700

    week01: add calculateStatus function

--- src/oop_92718_RyannChandiari/week01/GradingSystem.kt
+++ src/oop_92718_RyannChandiari/week01/GradingSystem.kt
@@ -1,14 +1,11 @@
 package oop_92718_RyannChandiari.week01
 
 fun main() {
-    // REFACTOR: Gunakan val (bukan var) dan hapus tipe data (: String/: Int)
     val name = "John Thor"
     val score = 80
 
-    // REFACTOR: Pakai String Template ($variable) biar lebih ringkas
     println("Nama: $name, Nilai: $score")
 
-    // Checkpoint 3: Tambahkan logika when sebagai Expression
     val grade = when (score) {
         in 90..100 -> "A"
         in 80..89 -> "B"
@@ -16,4 +13,10 @@ fun main() {
         else -> "D"
     }
     println("Grade kamu: $grade")
+
+    // Panggil fungsi status di sini
+    println("Status: ${calculateStatus(score)}")
 }
+
+// Fungsi dibuat DI LUAR main()
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
