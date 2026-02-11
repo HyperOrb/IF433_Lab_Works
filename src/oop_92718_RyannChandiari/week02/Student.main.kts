4c19b7a8a040f3f7ea51e8b0cd4b89db55aa58f8
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-11 10:18:34 +0700

    week02: create basic Student class structure

---
+++ src/oop_92718_RyannChandiari/week02/Student.main.kts
@@ -0,0 +1,11 @@
+#!/usr/bin/env kotlin
+
+package oop_92718_RyannChandiari.week02
+
+class Student(
+    val name: String,
+    val nim: String,
+    var major: String
+) {
+    // Body kosong dulu
+}
