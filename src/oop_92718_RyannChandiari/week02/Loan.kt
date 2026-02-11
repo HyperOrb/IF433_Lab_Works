bb78f37242a8bbff3abda84473a454824c7aeda0
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-11 10:23:03 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_92718_RyannChandiari/week02/Loan.kt
@@ -0,0 +1,15 @@
+package oop_92718_RyannChandiari.week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1 // Default 1 hari
+) {
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
