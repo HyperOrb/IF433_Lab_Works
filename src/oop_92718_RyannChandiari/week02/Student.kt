2197701718eab5b14d04a3ee1392f8b7c2d58483
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-11 10:21:54 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_92718_RyannChandiari/week02/Student.kt
+++ src/oop_92718_RyannChandiari/week02/Student.kt
@@ -3,9 +3,11 @@ package oop_92718_RyannChandiari.week02
 class Student(
     val name: String,
     val nim: String,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0 // Tambahan Checkpoint 5 (Default Argument)
 ) {
     init {
+        // ... (kode init sama seperti sebelumnya, tidak berubah)
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
@@ -14,7 +16,7 @@ class Student(
         }
     }
 
-    // Secondary Constructor (Checkpoint 4)
+    // Secondary Constructor (Tidak perlu diubah karena gpa punya default value)
     constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
         println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
