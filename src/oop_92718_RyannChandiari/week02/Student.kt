9c68fa9adcef8114b31f3a78a984c86be254859a
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-11 10:20:45 +0700

    week02: add secondary constructor

--- src/oop_92718_RyannChandiari/week02/Student.kt
+++ src/oop_92718_RyannChandiari/week02/Student.kt
@@ -13,4 +13,9 @@ class Student(
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
     }
+
+    // Secondary Constructor (Checkpoint 4)
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
 }
