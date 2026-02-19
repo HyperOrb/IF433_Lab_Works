3b25133a5c4e7824b58189d5446b44b54ad1eacd
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-19 13:18:10 +0700

    week03: simulate stack overflow error in setter

--- src/oop_92718_RyannChandiari/week03/Employee.kt
+++ src/oop_92718_RyannChandiari/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_92718_RyannChandiari.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value // PERINGATAN: INI BIKIN ERROR (Recursive)
+        }
 }
---
+++ src/oop_92718_RyannChandiari/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_92718_RyannChandiari.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
