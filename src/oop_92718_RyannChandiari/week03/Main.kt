b75c6f85e4b2c3dae4a87aa5b16683aa92bc266d
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-19 13:36:37 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_92718_RyannChandiari/week03/Main.kt
+++ src/oop_92718_RyannChandiari/week03/Main.kt
@@ -14,4 +14,17 @@ fun main() {
 
     // Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    // --- TUGAS MANDIRI 1: WEAPON ---
+    println("\n--- Test Weapon ---")
+    val sword = Weapon("Excalibur")
+
+    // Coba set damage ke -50 (harus gagal dan print peringatan)
+    sword.damage = -50
+
+    // Coba set damage ke 9999 (harus dipaksa mentok di 1000)
+    sword.damage = 9999
+
+    // Print hasil akhir beserta Tier-nya
+    println("Senjata: ${sword.name}, Damage: ${sword.damage}, Tier: ${sword.tier}")
 }
