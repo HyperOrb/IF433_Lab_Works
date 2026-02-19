3ff39be8d7e3073b70cd706de64ed0ced18b4e5f
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-19 13:39:13 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_92718_RyannChandiari/week03/Main.kt
+++ src/oop_92718_RyannChandiari/week03/Main.kt
@@ -1,30 +1,31 @@
 package oop_92718_RyannChandiari.week03
 
 fun main() {
+    // --- GUIDED LAB: EMPLOYEE ---
     val e = Employee("Budi")
-
-    // Test Validasi Salary
     e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
-
-    // Test Encapsulation
     e.increasePerformance()
     e.printStatus()
-
-    // Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 
     // --- TUGAS MANDIRI 1: WEAPON ---
     println("\n--- Test Weapon ---")
     val sword = Weapon("Excalibur")
-
-    // Coba set damage ke -50 (harus gagal dan print peringatan)
     sword.damage = -50
-
-    // Coba set damage ke 9999 (harus dipaksa mentok di 1000)
     sword.damage = 9999
-
-    // Print hasil akhir beserta Tier-nya
     println("Senjata: ${sword.name}, Damage: ${sword.damage}, Tier: ${sword.tier}")
+
+    // --- TUGAS MANDIRI 2: PLAYER ---
+    println("\n--- Test Player ---")
+    val p1 = Player("RyannZ")
+
+    // p1.xp = 100 // Coba hapus komen (//) di awal baris ini, pasti error karena xp itu private!
+
+    println("Player ${p1.username} dibuat. Level awal: ${p1.level}")
+    p1.addXp(50) // Tambah 50 XP (Total 50, Masih Level 1)
+    p1.addXp(60) // Tambah 60 XP (Total 110, Naik ke Level 2!)
+
+    println("Level akhir: ${p1.level}")
 }
