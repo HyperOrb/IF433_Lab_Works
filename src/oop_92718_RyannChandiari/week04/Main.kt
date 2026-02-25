56147b33edca37b967a9ab0ed8be36afbc80ba9f
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-25 10:07:29 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_92718_RyannChandiari/week04/Main.kt
+++ src/oop_92718_RyannChandiari/week04/Main.kt
@@ -18,4 +18,15 @@ fun main() {
     myEv.accelerate() // Memanggil method yang sudah di-final-kan
     myEv.honk()       // Memanggil method turunan dari Car
     myEv.openTrunk()  // Memanggil method turunan dari Car
+
+    // --- TAMBAHAN TUGAS MANDIRI 2 ---
+    println("\n--- Testing Employee Hierarchy ---")
+
+    val boss = Manager("Pak Budi", 10000000)
+    boss.work()
+    println("Bonus Manager: Rp ${boss.calculateBonus()}")
+
+    val dev = Developer("Ryann", 8000000, "Swift")
+    dev.work()
+    println("Bonus Developer: Rp ${dev.calculateBonus()}")
 }
