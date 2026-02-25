673fdf2d1be0e5d1822611badc7c48a6d73ce537
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-02-25 10:05:31 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_92718_RyannChandiari/week04/Main.kt
+++ src/oop_92718_RyannChandiari/week04/Main.kt
@@ -11,4 +11,11 @@ fun main() {
     myCar.openTrunk()
     myCar.honk()
     myCar.accelerate()
+
+    // --- TAMBAHAN TUGAS MANDIRI 1 ---
+    println("\n--- Testing ElectricCar ---")
+    val myEv = ElectricCar(brand = "Tesla", numberOfDoors = 4, batteryCapacity = 85)
+    myEv.accelerate() // Memanggil method yang sudah di-final-kan
+    myEv.honk()       // Memanggil method turunan dari Car
+    myEv.openTrunk()  // Memanggil method turunan dari Car
 }
