8374675dc57939acad5d59f37060cc097e41b2dd
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-03-11 09:52:11 +0700

    week06: create compound interfaces

---
+++ src/oop_92718_RyannChandiari/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_92718_RyannChandiari.week06
+
+// Interface: Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
