49567ca81a5f432de56dbab9fe84c8f61db93d33
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-03-11 09:58:50 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_92718_RyannChandiari/week06/Main.kt
+++ src/oop_92718_RyannChandiari/week06/Main.kt
@@ -7,19 +7,42 @@ fun processCheckout(method: PaymentMethod, amount: Double) {
 }
 
 fun main() {
-    // Test Smartwatch (Multiple Implementations)
+    // --- LATIHAN TERBIMBING ---
     val myWatch = Smartwatch()
     myWatch.showTime()
 
-    // Test Smartphone (Diamond Problem Resolution)
     val myPhone = Smartphone()
     myPhone.turnOn()
 
-    // Test Pembayaran (Decoupling)
     val pay1 = Gopay()
     val pay2 = CreditCard()
 
     println("\n=== TESTING CHECKOUT ===")
     processCheckout(method = pay1, amount = 50000.0)
     processCheckout(method = pay2, amount = 150000.0)
+
+    // --- TUGAS MANDIRI: SMART HOME ---
+    println("\n=== TESTING SMART HOME SYSTEM ===")
+
+    // Checkpoint 19: Instansiasi Perangkat
+    val lamp = SmartLamp("LMP-01", "Ruang Tamu")
+    val speaker = SmartSpeaker("SPK-01", "Google Nest Dapur")
+    val cctv = SmartCCTV("CAM-01", "Ezviz Garasi")
+
+    // Checkpoint 20: Instansiasi Hub dan Testing Logika
+    val hub = SmartHomeHub()
+
+    // Tambahkan semua perangkat ke Hub
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    println("\n--- Menyalakan Perangkat Manual ---")
+    lamp.turnOn()
+    speaker.turnOn()
+    cctv.turnOn() // Harusnya langsung otomatis merekam!
+
+    // Uji fitur canggih Smart Hub (Polymorphism & Smart Casting)
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }
