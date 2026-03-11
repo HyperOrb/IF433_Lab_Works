8feb9e17a33dbcf1bd03295db81dfd67e9d43503
Ryann Chandiari <ryann.chandiari@gmail.com>
2026-03-11 09:53:46 +0700

    week06: (trial) trigger the diamond problem compiler error

---
+++ src/oop_92718_RyannChandiari/week06/Smartphone.kt
@@ -0,0 +1,6 @@
+package oop_92718_RyannChandiari.week06
+
+// ERROR: Class Smartphone inherits multiple implementations of turnOn()
+class Smartphone : Camera, Phone {
+    // Sengaja dibiarkan kosong untuk melihat error Diamond Problem
+}
