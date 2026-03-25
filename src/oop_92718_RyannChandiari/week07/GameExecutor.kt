3a78de6bcb8cd80e7f55c255ead8397ad73879c6
Ryann <ryann.chandiari@email.com>
2026-03-25 10:12:43 +0700

    week07: (task) implement exhaustive processEvent

---
+++ src/oop_92718_RyannChandiari/week07/GameExecutor.kt
@@ -0,0 +1,10 @@
+package oop_92718_RyannChandiari.week07
+
+fun processEvent(event: BattleState) {
+    when (event) {
+        is BattleState.MonsterEncounter -> println("Bertemu monster: ${event.monsterName}")
+        is BattleState.LootDropped -> println("Loot Drop: ${event.item.name} (${event.item.rarity})")
+        is BattleState.GameOver -> println("Game Over! Alasan: ${event.reason}")
+        BattleState.SafeZone -> println("Berada di zona aman.")
+    }
+}
