647dbd606a66f1b7f7c8cd67e927141af27c60fc
Ryann <ryann.chandiari@email.com>
2026-03-25 09:55:32 +0700

    week07: (trial) trigger private constructor error on NetworkClient

---
+++ src/oop_92718_RyannChandiari/week07/Main.kt
@@ -0,0 +1,6 @@
+package oop_92718_RyannChandiari.week07
+
+fun main() {
+    // ERROR: Cannot access '<init>': it is private in 'NetworkClient'
+    val client = NetworkClient("https://api.umn.ac.id")
+}
