ef47c08fab40bdb9a3dd727827c772acc3772365
Ryann <ryann.chandiari@email.com>
2026-04-15 09:57:29 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_92718_RyannChandiari/week08/ApiParser.kt
@@ -0,0 +1,26 @@
+package oop_92718_RyannChandiari.week08
+
+class ApiParser {
+    fun parseProduct(rawJson: Map<String, Any?>): Product? {
+        // Ekstrak ID dan Name (Wajib). Kalau null, langsung throw Exception.
+        val id = requireNotNull(rawJson["id"]) { "API Invalid: Missing ID" }.toString()
+        val name = requireNotNull(rawJson["name"]) { "API Invalid: Missing Name" }.toString()
+
+        // Ekstrak tipe sebagai String
+        val type = rawJson["type"] as? String
+
+        return when (type) {
+            "ELECTRONIC" -> {
+                // Ekstrak warranty. Jika gagal jadi Int atau null, fallback ke 12
+                val warranty = rawJson["warranty"] as? Int ?: 12
+                Product.Electronic(id, name, warranty)
+            }
+            "CLOTHING" -> {
+                // Ekstrak size. Jika gagal jadi String atau null, fallback ke "All Size"
+                val size = rawJson["size"] as? String ?: "All Size"
+                Product.Clothing(id, name, size)
+            }
+            else -> null // Tipe tidak dikenal, return null
+        }
+    }
+}
