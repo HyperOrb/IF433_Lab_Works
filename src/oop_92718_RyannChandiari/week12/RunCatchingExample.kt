850be3df0aa40a578810b1d80a53f7082126f8c1
Ryann <ryann.chandiari@email.com>
2026-05-13 09:53:42 +0700

    week12: implement basic runCatching

---
+++ src/oop_92718_RyannChandiari/week12/RunCatchingExample.kt
@@ -0,0 +1,24 @@
+package oop_92718_RyannChandiari.week12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
+
+    // Pattern: getOrElse
+    val safeValue = result.getOrElse { -1 }
+    println("Safe Value (getOrElse): $safeValue")
+
+    // Pattern: recover (bisa mengubah tipe kegagalan jadi sukses)
+    val recovered = result.recover { 0 }.getOrNull()
+    println("Recovered Value: $recovered")
+
+    runCatching {
+        "100".toInt()
+    }.onSuccess { v ->
+        println("Berhasil dikonversi: $v")
+    }.onFailure { e ->
+        println("Gagal konversi: ${e.message}")
+    }
+}
